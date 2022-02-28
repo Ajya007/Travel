e0265cee4ac437a8f46890628e499cdabad91a80
ajya <ajay.codehimalaya@gmail.com>
2022-02-28 14:13:53 +0545

    showcaseFeilg

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-const hamburger=document.querySelector(".menu-btn");
-const nav=document.querySelector("nav");
-const navChild=document.querySelectorAll(".nav-wrapper a")
+const hamburger = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav-end");
+const navChild = document.querySelectorAll(".nav-wrapper a")
 
 navChild.forEach(el => {
-  el.addEventListener('click',toggleMenu);
+  el.addEventListener('click', toggleMenu);
 })
 
-hamburger.addEventListener('click',toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
-let showMenu=false;
+let showMenu = false;
 
 
-function toggleMenu(){
-    showMenu=!showMenu
-    if(showMenu){
-        hamburger.classList.add('close')
-        nav.classList.add('close')
+function toggleMenu() {
+  showMenu = !showMenu
+  if (showMenu) {
+    hamburger.classList.add('close')
+    nav.classList.add('close')
 
-    }
-    else{
-        hamburger.classList.remove('close')
-        nav.classList.remove('close')
-    }
+  }
+  else {
+    hamburger.classList.remove('close')
+    nav.classList.remove('close')
+  }
 
 
 }
@@ -32,13 +32,13 @@ function toggleMenu(){
 // var reswidth =screen.width
 // if(reswidth>467){
 //   document.querySelector(".slider").classList.add("regular")
-  
+
 // }else{
 //   document.querySelector(".slider").classList.remove("regular")
 
 // }
 
-	function topFunction() {
+function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
@@ -47,11 +47,11 @@ function toggleMenu(){
 
 //for image change
 
-var ProductImg=document.getElementById("productImg")
+var ProductImg = document.getElementById("productImg")
 
 
-function oe(id,src){
-  ProductImg.src=src
+function oe(id, src) {
+  ProductImg.src = src
 }
 
 
