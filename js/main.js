5a476863dbb04fee18ae2bc87ceb0deac08ce577
ajya <ajay.codehimalaya@gmail.com>
2022-03-01 10:14:12 +0545

    adding destinations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 const hamburger = document.querySelector(".menu-btn");
 const nav = document.querySelector(".nav-end");
 const navChild = document.querySelectorAll(".nav-wrapper a")
+const card = document.querySelectorAll(".card-wrapper")
+// const pink = document.querySelector('.pink')
+
+
+
+card.forEach(el => {
+  el.addEventListener('mouseenter', cardToggler)
+})
+card.forEach(el => {
+  el.addEventListener('mouseleave', cardToggler)
+})
+let cardtoggle = false
+function cardToggler(e) {
+  let pink = e.target.children[1]
+  pink.classList.toggle("show")
+}
+
+
+
+
 
 navChild.forEach(el => {
   el.addEventListener('click', toggleMenu);
